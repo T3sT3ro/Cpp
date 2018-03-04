79408dfb61ad88c4e165132a33955445ed840df4
Tooster <max@polarczyk.pl>
2018-03-04 19:29:48 +0100

    L2 Bugfix for tests

--- L2/main.cpp
+++ L2/main.cpp
@@ -46,50 +46,50 @@ int main() {
 
     for (int i = 0; i < 10000000; ++i) { // 1000 randomised tests
         double r[6];
+        for (auto &it : r)
+            it = std::rand() % 1000;
+
+        Punkt *_a = new Punkt(r[0], r[1]);
+        Punkt *_b = new Punkt(r[2], r[3]);
+        Punkt *_c = new Punkt(r[4], r[5]);
+
         try {
-            for (auto &it : r)
-                it = std::rand() % 1000;
-
-            delete (a);
-            delete (b);
-            delete (c);
-
-            a = new Punkt(r[0], r[1]);
-            b = new Punkt(r[2], r[3]);
-            c = new Punkt(r[4], r[5]);
 
             // adding vectors
             assert(Wektor::add({r[0], r[1]}, {r[2], r[3]}) == Wektor(r[0] + r[2], r[1] + r[3]));
 
-            delete (p1);
 
-            p1 = new Prosta(*a, *b);
+            Prosta *_p = new Prosta(*_a, *_b);
 
             // check if points on which line was created really belong to the line
-            assert(p1->contains(*a) && p1->contains(*b));
+            assert(_p->contains(*_a) && _p->contains(*_b));
 
             // tests creation of line based on vector
-            delete (p1);
-            p1 = new Prosta(Wektor(a->x, a->y));
-            assert(p1->contains(Punkt(Punkt({0, 0}, Wektor(a->y, a->x * (-1))) * r[3], Wektor(a->x, a->y))));
+            delete (_p);
+            _p = new Prosta(Wektor(_a->x, _a->y));
+            assert(_p->contains(Punkt(Punkt({0, 0}, Wektor(_a->y, _a->x * (-1))) * r[3], Wektor(_a->x, _a->y))));
 
             // check if line is in normalised form
-            assert(std::abs(p1->getA() * p1->getA() + p1->getB() * p1->getB() - 1.00000000) < epsilon);
+            assert(std::abs(_p->getA() * _p->getA() + _p->getB() * _p->getB() - 1.00000000) < epsilon);
 
             // check for parallel and perpendicular lines
-            assert(Prosta::parallel(Prosta(*a, *b),
-                                    Prosta(Punkt(*a, Wektor(c->x, c->y)), Punkt(*b, Wektor(c->x, c->y)))));
-            assert(Prosta::perpendicular(Prosta({0, 0}, *a), Prosta(Wektor(a->x, a->y))));
+            assert(Prosta::parallel(Prosta(*_a, *_b),
+                                    Prosta(Punkt(*_a, Wektor(_c->x, _c->y)), Punkt(*_b, Wektor(_c->x, _c->y)))));
+            assert(Prosta::perpendicular(Prosta({0, 0}, *_a), Prosta(Wektor(_a->x, _a->y))));
 
             // check intersection
-            Punkt inter = Prosta::intersection(Prosta({0, 0}, *a), Prosta(Wektor(a->x, a->y)));
-            assert(std::isnan(inter.x) || std::abs(inter.x - a->x) < epsilon && std::abs(inter.y - a->y) < epsilon);
+            Punkt inter = Prosta::intersection(Prosta({0, 0}, *_a), Prosta(Wektor(_a->x, _a->y)));
+            assert(std::isnan(inter.x) || std::abs(inter.x - _a->x) < epsilon && std::abs(inter.y - _a->y) < epsilon);
+            delete (_p);
         } catch (std::invalid_argument &e) {
             std::cerr << "Captured exception for randoms: ";
             for (auto const &_r : r)
-                std::cerr << _r << "   ";
-            std::cerr << " >> " << e.what();
+                std::cerr << _r << " ";
+            std::cerr << " >> " << e.what() << std::endl;
         }
+        delete (_a);
+        delete (_b);
+        delete (_c);
     }
 
 
